80f9ccd31b2c8948e0a72a92bdd4422320304f76
Pedro Henrique de Lacerda <pedroinc@gmail.com>
2020-05-01 21:15:33 -0300

    fist commit: template

---
+++ src/app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors");
+
+// const { uuid } = require("uuidv4")
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const repositories = [];
+
+app.get("/repositories", (req, res) => {
+    // TODO
+});
+
+app.post("/repositories", (req, res) => {
+    // TODO
+});
+
+app.put("/repositories/:id", (req, res) => {
+    // TODO
+});
+
+app.delete("/repositories/:id", (req, res) => {
+
+});
+
+module.exports = app;
